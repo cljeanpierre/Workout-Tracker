6a2db22ce0d542712616033d3ddefd92c3289ecf
Carla <carla@layc-dc.org>
2020-12-31 16:03:57 -0500

    created serverjs and named constants for database and collections

--- Develop/seeders/server.js
+++ Develop/seeders/server.js
@@ -16,3 +16,8 @@ const databaseUrl = "fitness";
 const collections = ["workouts"];
 
 const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+    console.log("Database Error:", error);
+  });
+  
