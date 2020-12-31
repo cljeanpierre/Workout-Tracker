1172e182a30cf56cee642a695af3ba9d4d816404
Carla <carla@layc-dc.org>
2020-12-31 16:15:44 -0500

    added routes in server file to get the home, stats, and exercise pages

--- Develop/seeders/server.js
+++ Develop/seeders/server.js
@@ -19,5 +19,16 @@ const db = mongojs(databaseUrl, collections);
 
 db.on("error", error => {
     console.log("Database Error:", error);
-  });
-  
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/stats.html"));
+});
