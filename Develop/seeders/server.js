9e1cbede80b344d2754545946aee6cef99aac06b
Carla <carla@layc-dc.org>
2020-12-31 17:57:00 -0500

    added functionality to add exercises to previous workout, add new exercises and view all of the stats

--- Develop/seeders/server.js
+++ Develop/seeders/server.js
@@ -19,8 +19,8 @@ app.use(express.static("public"));
 const data = {};
 
 Workout.create(data)
-.then(dbFitness => {
-    console.log(dbFitness);
+.then(dbWorkout => {
+    console.log(dbWorkout);
   })
   .catch(({ message }) => {
     console.log(message);
@@ -48,8 +48,48 @@ app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname + "./public/stats.html"));
 });
 
-//add function to update exercises to a previous workout plan
 
-//add function to create new exercises to a new workout plan
+//add function to update exercises to a previous workout plan
+app.post("/update/id", (req, res) => {
+    db.fitness.update(
+      {
+        _id: mongojs.ObjectId(req.params.id)
+      },
+      {
+        $set: {
+
+        }
+      },
+      (error, data) => {
+        if (error) {
+          res.send(error);
+        } else {
+          res.send(data);
+        }
+      }
+    );
+  });
+
+  //add function to create new exercises to a new workout plan
+app.post("/submit", (req, res) => {
+    console.log(req.body);
+  
+    db.notes.insert(req.body, (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    });
+  });
 
 //add function to view the weight of all of the exercises on the stats page
+app.get("/all", (req, res) => {
+    db.fitness.find({}, (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.json(data);
+      }
+    });
+  });
