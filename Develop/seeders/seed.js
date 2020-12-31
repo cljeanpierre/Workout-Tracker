080b23d13fa773fc4956bedab3da9e840de7efd6
Carla <carla@layc-dc.org>
2020-12-31 12:42:43 -0500

    deploying

--- Develop/seeders/seed.js
+++ Develop/seeders/seed.js
@@ -1,6 +1,7 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
+//locate environment variable 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/fitness', 
   {
