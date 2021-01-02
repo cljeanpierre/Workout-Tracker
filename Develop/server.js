b69617e61b312a91ed8e65a1e55534054a534874
Carla <carla@layc-dc.org>
2021-01-01 21:28:17 -0500

    added gitignore file to disregard node_modules in repository

---
+++ Develop/server.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+const path = require("path");
+const mongoose = require("mongoose");
+const Workout = require("./models/workout.js");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbExample", { useNewUrlParser: true });
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const data = {};
+
+Workout.create(data)
+.then(dbWorkout => {
+    console.log(dbWorkout);
+  })
+  .catch(({ message }) => {
+    console.log(message);
+  });
+
+
+const databaseUrl = "fitness";
+const collections = ["workouts"];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+    console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
+app.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/exercise.html"));
+});
+
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname + "./public/stats.html"));
+});
+
+
+//add function to update exercises to a previous workout plan
+app.post("/update/id", (req, res) => {
+    db.fitness.update(
+      {
+        _id: mongojs.ObjectId(req.params.id)
+      },
+      {
+        $set: {
+
+        }
+      },
+      (error, data) => {
+        if (error) {
+          res.send(error);
+        } else {
+          res.send(data);
+        }
+      }
+    );
+  });
+
+  //add function to create new exercises to a new workout plan
+app.post("/submit", (req, res) => {
+    console.log(req.body);
+  
+    db.notes.insert(req.body, (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(data);
+      }
+    });
+  });
+
+//add function to view the weight of all of the exercises on the stats page
+app.get("/stats", (req, res) => {
+    db.fitness.findAll({}, (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.json(data);
+      }
+    });
+  });
