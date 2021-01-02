ccca62c8c4cd8fbd23f28fabd7c36976dbd31d7e
Carla <carla@layc-dc.org>
2021-01-01 23:12:27 -0500

    added express router to post and view exercises to workout plan, exported router

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -3,46 +3,48 @@ const Workout = require("../models/workout.js");
 const path = require("path");
 
 //add function to update exercises to a previous workout plan
-app.post("/update/id", (req, res) => {
+router.post("/api/workout", (req, res) => {
     db.fitness.update(
-      {
-        _id: mongojs.ObjectId(req.params.id)
-      },
-      {
-        $set: {
+        {
+            _id: mongojs.ObjectId(req.params.id)
+        },
+        {
+            $set: {
 
+            }
+        },
+        (error, data) => {
+            if (error) {
+                res.send(error);
+            } else {
+                res.send(data);
+            }
         }
-      },
-      (error, data) => {
-        if (error) {
-          res.send(error);
-        } else {
-          res.send(data);
-        }
-      }
     );
-  });
+});
 
-  //add function to create new exercises to a new workout plan
-app.post("/submit", (req, res) => {
+//add function to create new exercises to a new workout plan
+router.post("/api/workout", (req, res) => {
     console.log(req.body);
-  
+
     db.notes.insert(req.body, (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
+        if (error) {
+            res.send(error);
+        } else {
+            res.send(data);
+        }
     });
-  });
+});
 
 //add function to view the weight of all of the exercises on the stats page
-app.get("/stats", (req, res) => {
+router.get("/api/workout", (req, res) => {
     db.fitness.findAll({}, (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.json(data);
-      }
+        if (error) {
+            res.send(error);
+        } else {
+            res.json(data);
+        }
     });
-  });
+});
+
+module.exports = router;
