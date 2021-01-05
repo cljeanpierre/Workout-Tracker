6c7e2d3e44f21868febb1b5d83428898c4861e50
Carla <carla@layc-dc.org>
2021-01-05 06:05:46 -0500

    updated code to accommodate MongoDB connection

--- Develop/routes/server.js
+++ Develop/routes/server.js
@@ -16,7 +16,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Fitness", 
+  { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+ );
 
 const data = {};
 
