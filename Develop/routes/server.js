f7c75b4600fa4ffa72b580bf7231f054638616d6
Carla <carla@layc-dc.org>
2021-01-08 16:15:14 -0500

    corrected erroneous path to locate homepage, exercise page, and stats page

--- Develop/routes/server.js
+++ Develop/routes/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const mongojs = require("mongojs");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
-const Workout = require("./public/workout.js");
+const Workout = require("../models/workout.js");
 
 const app = express();
 
@@ -16,7 +17,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Fitness", 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Fitnessdb", 
   { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -35,7 +36,7 @@ Workout.create(data)
     console.log(message);
   });
 
-const databaseUrl = "fitness";
+const databaseUrl = "Fitness";
 const collections = ["workouts"];
 
 const db = mongojs(databaseUrl, collections);
@@ -45,15 +46,61 @@ db.on("error", error => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/index.html"));
+    res.sendFile(path.join(__dirname + "/../public/index.html"));
 });
 
 app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/exercise.html"));
+    res.sendFile(path.join(__dirname + "/../public/exercise.html"));
 });
 
 app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname + "./public/stats.html"));
+    res.sendFile(path.join(__dirname + "/../public/stats.html"));
+});
+
+
+//add function to update exercises to a previous workout plan
+app.post("/api/workouts", (req, res) => {
+  db.fitness.update(
+      {
+          _id: mongojs.ObjectId(req.params.id)
+      },
+      {
+          $set: {
+
+          }
+      },
+      (error, data) => {
+          if (error) {
+              res.send(error);
+          } else {
+              res.send(data);
+          }
+      }
+  );
+});
+
+//add function to create new exercises to a new workout plan
+app.post("/api/workouts", (req, res) => {
+  console.log(req.body);
+
+  db.notes.insert(req.body, (error, data) => {
+      if (error) {
+          res.send(error);
+      } else {
+          res.send(data);
+      }
+  });
+});
+
+//add function to view the weight of all of the exercises on the stats page
+app.get("/api/workouts", (req, res) => {
+  db.fitness.findAll({}, (error, data) => {
+      if (error) {
+          res.send(error);
+      } else {
+          res.json(data);
+      }
+  });
 });
 
 app.listen(PORT, () => {
