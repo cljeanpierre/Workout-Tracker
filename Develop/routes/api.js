759b5919cfecd1d97a19dee2be66d71ddf77d1d9
Carla <carla@layc-dc.org>
2021-01-10 09:06:15 -0500

    required express router and removed put, post, and get routes from serverjs file to go here

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,4 +1,56 @@
-const Workout = require("../models/workout.js");
-const path = require("path");
+const router = require('express').Router();
+const Workout = require('../models/workout');
 
+router.post('/api/workouts', (req, res)=>{
+    Workout.create({})
+    .then((dbworkout) => {
+        res.json(dbworkout)
+    })
+    .catch((err) => {
+        res.json(err);
+    })
+})
 
+//function to update exercises to a previous workout plan
+router.put('/api/workouts/:id', ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercises: body } },
+      // "runValidators" ensures new exercises meet schema requirements
+      { new: true, runValidators: true }
+    )
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+//router.get()
+//function to create new exercises to a new workout plan
+router.post("/api/workouts", (req, res) => {
+  console.log(req.body);
+
+  db.notes.insert(req.body, (error, data) => {
+      if (error) {
+          res.send(error);
+      } else {
+          res.send(data);
+      }
+  });
+});
+
+//router.get()
+//function to view the weight of all of the exercises on the stats page
+router.get("/api/workouts", (req, res) => {
+  db.fitness.findAll({}, (error, data) => {
+      if (error) {
+          res.send(error);
+      } else {
+          res.json(data);
+      }
+  });
+});
+
+module.exports = router;
